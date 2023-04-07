35850dae703925e8f6afcf2d6fb954a6dc59f530
Elwin Roman <elwinromanv@gmail.com>
2023-04-07 01:40:41 -0500

    Renombra export functions

--- js/config.js
+++ js/config.js
@@ -1,11 +1,8 @@
-const CELL_SIZE = 12
-const CELL_COLOR_ACTIVE = '#00517B'
-const CELL_COLOR_DEACTIVE = '#fff' 
+export const CELL_SIZE = 8
+export const CELL_COLOR_ACTIVE = '#00517B'
+export const CELL_COLOR_DEACTIVE = '#fff' 
 export const COLUMN_MATRIX = 100 // tamaño de la matriz en columnas
 export const ROW_MATRIX = 100    // tamaño de la matriz en filas
 export const CELL_ON = 1         // celula viva
 export const CELL_OFF = 0        // celula muerta
-
-export {
-   CELL_SIZE, CELL_COLOR_ACTIVE, CELL_COLOR_DEACTIVE
-}
+export const SPEED = 100         // velocidad (miliseconds)
