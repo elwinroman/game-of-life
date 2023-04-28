82eb250b1ae27ddfc60cea88725cc429b18102e1
Elwin Roman <elwinromanv@gmail.com>
2023-04-28 15:22:14 -0500

    Agrega estilos css
    - Sección "game"
    - SVGs
    - Select pattern
    - Input-range
    - Colores globales para light-mode y dark-mode

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ export const ALIVE = 1         // celula viva
 export const DEAD = 0        // celula muerta
 export const SPEED = 100         // velocidad (miliseconds)
 export const COLOR = {
-   gridline: '#9FADBD',
-   alive_cell: '#00517B',
+   gridline: '#98A6AD',
+   alive_cell: '#2F343B',
    background: '#FFF' 
 }
