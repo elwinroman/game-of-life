8ec09fb417c7a1cac5499935d034d5c69e22df9a
Elwin Roman <elwinromanv@gmail.com>
2023-05-14 19:45:59 -0500

    Aumenta el universo del juego

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 export const CELL_SIZE = 8
 export const COLUMN_MATRIX = 100 // tamaño de la matriz en columnas
 export const ROW_MATRIX = 100    // tamaño de la matriz en filas
+export const COLUMN_MATRIX = 2000 // tamaño de la matriz en columnas
+export const ROW_MATRIX = 2000    // tamaño de la matriz en filas
 export const ALIVE = 1         // celula viva
 export const DEAD = 0        // celula muerta
 export const SPEED = 100         // velocidad (miliseconds)
